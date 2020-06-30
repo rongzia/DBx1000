9376ca79fef3aa41547be9fa1eb226dd36ca1ca5
rrzhang <871458417@qq.com>
2020-06-30 16:02:15 +0800

    temp

--- run.sh
+++ run.sh
@@ -4,12 +4,15 @@ pwd
 # $1 为第 i 个节点的编号
 # ./run 1 8, 表示第 2 个结点，8个进程
 
-echo "node_id     : $1"
-echo "process_num : $2"
-start=`expr $1 \* $2`
-temp=`expr $1 + 1`
-temp_end=`expr $temp \* $2`
-end=`expr $temp_end - 1`
+#echo "node_id     : $1"
+#echo "process_num : $2"
+#start=`expr $1 \* $2`
+#temp=`expr $1 + 1`
+#temp_end=`expr $temp \* $2`
+#end=`expr $temp_end - 1`
+
+start=$1
+end=`expr $2 - 1`
 
 echo "start : $start"
 echo "end   : $end"
